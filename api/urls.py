6bf73ef9b0e130fce49c71dc59e315e0baea6b14
Emeth <seanybob@gmail.com>
2015-09-12 22:13:30 -0500

    cleanup

--- api/urls.py
+++ api/urls.py
@@ -3,18 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^get_info_special$', "api.views.get_info"),
 
-    url(r'^auth_first_step$', "api.views.auth_first_step"),
-    
-    url(r'^auth_second_step$', "api.views.auth_second_step"),
-
-    url(r'^welcome$', "api.views.welcome"),
-
-    url(r'^twilio$', "api.views.twilio"),
-    
-    url(r'^twilio_text$', "api.views.twilio_text"),
-    
-    url(r'^welcome_text$', "api.views.welcome_text"),
-
     url(r'^contact_received$', "api.views.contact_received"),
     
     url(r'^contact_received_voice$', "api.views.contact_received_voice"),
--- api/views.py
+++ api/views.py
@@ -322,11 +322,6 @@ def contact_received_voice(request):
         exc_type, exc_value, exc_traceback = sys.exc_info()
         print exc_value.message
 
-def welcome(request):
-    return HttpResponse(json.dumps({
-            "message": "Welcome to the St. Louis Regional Municipal Court System Helpline! Please enter your citation number or driver's license number."
-        }, default=json_custom_parser), content_type='application/json', status=200)
-
 def get_info(request):
 
     if request.GET.get('citation', False) and request.GET.get('last_name', False) and request.GET.get('date_of_birth', False):
@@ -338,7 +333,7 @@ def get_info(request):
         citation_in_db = Citation.objects.filter(drivers_license_number=request.GET['drivers_license_number']).filter(last_name=request.GET['last_name']).filter(date_of_birth=parser.parse(request.GET['date_of_birth']))
 
     else:
-        
+
         return HttpResponse(json.dumps({
             "status": "error",
             "message": "Not enough information to authenticate user. Please pass in Date of Birth, Last Name, AND either driver's license number or citation number."
@@ -370,99 +365,4 @@ def get_info(request):
             "status": "error",
             "message": "Citation not found in database."
         }, default=json_custom_parser), content_type='application/json', status=200)
-
-
-
-def auth_first_step(request):
-    
-    if request.GET.get('citation', False):
-
-        citation_in_db = Citation.objects.filter(citation_number=request.GET['citation'])
-
-    elif request.GET.get('drivers_license_number', False):
-
-        citation_in_db = Citation.objects.filter(drivers_license_number=request.GET['drivers_license_number'])
-
-    else:
-        
-        return HttpResponse(json.dumps({
-            "status": "error",
-            "message": "Citation Number or Driver's License not found in database."
-        }, default=json_custom_parser), content_type='application/json', status=200)
-
-
-    if citation_in_db.exists():
-      
-        return HttpResponse(json.dumps({
-            "status": "success",
-            "message": "Your Citation Number has been confirmed. Please send your last name and your date of birth."
-        }, default=json_custom_parser), content_type='application/json', status=200)
-    else:
-        #return error, not found
-        return HttpResponse(json.dumps({
-            "status": "error",
-            "message": "Citation not found in database."
-        }, default=json_custom_parser), content_type='application/json', status=200)
-
-
-    
-def auth_second_step(request):
-    if request.GET.get('last_name', False) and request.GET.get('date_of_birth', False):
-
-        citation_in_db = Citation.objects.filter(last_name=request.GET['last_name']).filter(date_of_birth=parser.parse(request.GET['date_of_birth']))
-
-    else:
-        
-        return HttpResponse(json.dumps({
-            "status": "error",
-            "message": "Not enough information to authenticate user. Please send your last name and date of birth."
-        }, default=json_custom_parser), content_type='application/json', status=200)
-
-
-    if citation_in_db.exists():
-        violation_in_db = Violation.objects.filter(citation_number=request.GET['citation'])
-        return HttpResponse(json.dumps({
-            "status": "success",
-            "citation": list(citation_in_db.values())[0],
-            "violation": list(violation_in_db.values())[0]
-        }, default=json_custom_parser), content_type='application/json', status=200)
-    else:
-        #return error, not found
-        return HttpResponse(json.dumps({
-            "status": "error",
-            "message": "Citation not found in database."
-        }, default=json_custom_parser), content_type='application/json', status=200)
-
-def twilio(request):
-    
-    twil = '''<?xml version="1.0" encoding="UTF-8"?>
-            <Response>
-                <Say>Twilio can speak text.</Say>
-                <Say voice="man">It can sound like a man.</Say>
-                <Say voice="woman">Or a woman.</Say>
-                <Say language="es">O habla en espanol.</Say>
-                <Say voice="woman">Tada!</Say>
-            </Response>'''
-    return HttpResponse(twil, content_type='application/xml', status=200)
-
-
-def twilio_text(request):
-    
-    twil = '''<?xml version="1.0" encoding="UTF-8"?>
-            <Response>
-                <Sms>Store Location: 123 Easy St. YO!</Sms>
-            </Response>
-            '''
-    return HttpResponse(twil, content_type='application/xml', status=200)
-
-
-def welcome_text(request):
-    
-    twil = '''<?xml version="1.0" encoding="UTF-8"?>
-            <Response>
-                <Gather>"Welcome to the St. Louis Regional Municipal Court System Helpline! Please enter your citation number or driver's license number."</Gather>
-            </Response>
-            '''
-    return HttpResponse(twil, content_type='application/xml', status=200)
-
 
