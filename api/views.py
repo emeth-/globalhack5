a686b40e8107865ad5379236f4a5eb5444ac6eba
Emeth <seanybob@gmail.com>
2015-09-13 09:51:06 -0500

    tweaks

--- api/views.py
+++ api/views.py
@@ -55,7 +55,7 @@ def contact_received(request):
                     request.session['citation_number'] = citation_in_db[0].citation_number
                     twil = '''<?xml version="1.0" encoding="UTF-8"?>
                             <Response>
-                                <Message method="GET">Your Citation Number has been confirmed. Please send your date of birth.</Message>
+                                <Message method="GET">Your citation has been found. To verify your identity, please enter your date of birth.</Message>
                             </Response>
                             '''
                     return HttpResponse(twil, content_type='application/xml', status=200)
@@ -114,9 +114,7 @@ def contact_received(request):
             
                 twil = '''<?xml version="1.0" encoding="UTF-8"?>
                         <Response>
-                            <Message method="GET">Thank you, that matches our records. Here is your ticket info!</Message>
-                            <Message method="GET">{ticket_info}</Message>
-                            <Message>For a list of violations, send 1. \nFor citation information, send 2. \nFor options on how to pay outstanding fines, send 3.</Message>
+                            <Message method="GET">{ticket_info} \n For a list of violations, send 1. For citation information, send 2. \nFor options on how to pay outstanding fines, send 3.</Message>
                         </Response>
                         '''
                 violations_in_db = Violation.objects.filter(citation_number=request.session['citation_number'])
@@ -131,7 +129,13 @@ def contact_received(request):
                         has_warrant = True
                 citation_obj['total_owed'] = total_owed
                 citation_obj['has_warrant'] = has_warrant
-                ticket_info = "Court Date: " + str(citation_in_db[0].court_date) + "\n\n" + "Court Location: " + str(citation_in_db[0].court_location) + "\n\n" + "Court Address: " + str(citation_in_db[0].court_address) + "\n\n" + "Total Amount Owed: $" + str(citation_obj['total_owed']) + "\n\n" + "Outstanding Warrant: " + str(citation_obj['has_warrant'])
+
+                ticket_info = "You have a court hearing on " + str(citation_in_db[0].court_date).split(" ")[0] + ", at " + str(citation_in_db[0].court_location) + ", located at " + str(citation_in_db[0].court_address) + " . "
+                if has_warrant:
+                    ticket_info += " You have an outstanding warrant. "
+                else:
+                    ticket_info += " You do not have an outstanding warrant. "
+                ticket_info += "You currently have an outstanding balance of " + str(total_owed) + " dollars. "
                 twil = twil.replace("{ticket_info}", ticket_info)
                 return HttpResponse(twil, content_type='application/xml', status=200)
             
@@ -151,7 +155,6 @@ def contact_received(request):
                     has_warrant = True
             citation_obj['total_owed'] = total_owed
             citation_obj['has_warrant'] = has_warrant
-            #ticket_info = "Court Date: " + str(citation_in_db[0].court_date) + " / Court Location: " + str(citation_in_db[0].court_location) + " / Court Address: " + str(citation_in_db[0].court_address)
             
             twil = '''<?xml version="1.0" encoding="UTF-8"?>
                     <Response>'''
@@ -159,21 +162,21 @@ def contact_received(request):
                 #break down in violations
                 for v in violations_in_db:
                     twil += '<Message> {violation_info}</Message>'
-                    violation_info = "Violation Description: " + str(v.violation_description) + "\n\n" + "Fine Amount: $" + str(v.fine_amount) + "\n\n" + "Court Cost: $" + str(v.court_cost) 
+                    violation_info = "Your violation is " + str(v.violation_description) + ", with a fine amount of $" + str(v.fine_amount) + " and a court cost of $" + str(v.court_cost)
                     twil = twil.replace('{violation_info}',violation_info)
                     
             elif sms_from_user == '2':
                 #citation information
                 twil += '<Message>{citation_info}</Message>'
-                citation_info = "Citation Number: " + str(citation_obj['citation_number']) +  "\n\n" + "Citation Date: " + str(citation_obj['citation_date'])
+                citation_info = "Your citation number is " + str(citation_obj['citation_number']) + ", and its date is " + str(citation_obj['citation_date']).split(' ')[0]
                 twil = twil.replace('{citation_info}',citation_info)
                 
             elif sms_from_user == '3':
                 #ticket payment
-                twil += "<Message>To pay by phone, call \n(877) 866-3926. \n\nTo pay in person, go to \nMissouri Fine Collection Center \nP.O. Box 104540 \nJefferson City, MO 65110 \n\nFor payment plans or to perform community service in lieu of payment, request a hearing.</Message>"    
+                twil += "<Message>To pay by phone, call (877) 866-3926. To pay in person, go to Missouri Fine Collection Center, P.O. Box 104540, Jefferson City, MO 65110. For payment plans or to perform community service in lieu of payment, request a hearing.</Message>"
                       
-            #else:
-            #    twil += "<Message>You have entered an invalid option.</Message>"
+            else:
+                twil += "<Message>You have entered an invalid option.</Message>"
 
             twil += """
                     </Response>
@@ -356,7 +359,7 @@ def contact_received_voice(request):
                     has_warrant = True
                 citation_obj['total_owed'] = total_owed
                 citation_obj['has_warrant'] = has_warrant
-                ticket_info = "You have a court hearing on " + str(citation_in_db[0].court_date) + ", at " + str(citation_in_db[0].court_location) + ", located at " + str(citation_in_db[0].court_address) + " . "
+                ticket_info = "You have a court hearing on " + str(citation_in_db[0].court_date).split(" ")[0] + ", at " + str(citation_in_db[0].court_location) + ", located at " + str(citation_in_db[0].court_address) + " . "
                 if has_warrant:
                     ticket_info += " You have an outstanding warrant. "
                 else:
